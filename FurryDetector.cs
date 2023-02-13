40c750c8c89176838b1d0fc76353c94f3cbd1af0
Daniel Viola <daniel.viola001@mymdc.net>
2023-02-13 13:57:56 -0500

    Update package, i did this last night but didn't push it to the repo

--- FurryDetector.cs
+++ FurryDetector.cs
@@ -4,9 +4,7 @@
     {
         public static bool IsAFurry(string text)
         {
-            if (text.ToLower().Contains("owo") || text.ToLower().Contains("uwu"))
-                return true;
-            else return false;
+            return text.ToLower().Contains("owo") || text.ToLower().Contains("uwu");
         }
     }
 }
